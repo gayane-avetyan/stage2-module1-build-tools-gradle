b578fc803879ca66c36ed98bc2f666cfd173bf52
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-11 02:31:29 +0400

    in isPositiveNumber method null check added

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str == null) {
+            return false;
+        }
+
         try {
             double num = Double.parseDouble(str);
             return num > 0;
