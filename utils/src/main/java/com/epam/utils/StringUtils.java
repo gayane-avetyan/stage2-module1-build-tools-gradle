c18c23294532fb9316defc0dd4cbb458388aff82
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-11 02:24:42 +0400

    isPositiveNumber method fixed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,10 +3,15 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            int num = Integer.parseInt(str);
+            double num = Double.parseDouble(str);
             return num > 0;
         } catch (NumberFormatException e) {
-            return false;
+            try {
+                int num = Integer.parseInt(str);
+                return num > 0;
+            } catch (NumberFormatException ex) {
+                return false;
+            }
         }
     }
 }
