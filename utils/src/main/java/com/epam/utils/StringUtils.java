74d29983869dd1f12b1b56b16700a05fc92b3a0f
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-13 19:54:07 +0400

    isPositiveNumber method

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -5,10 +5,10 @@ public class StringUtils {
         if (str == null) {
             return false;
         }
-
         try {
             int num = Integer.parseInt(str);
-            return num > 0;
+            int firstLetter =  Integer.parseInt(str.substring(0, 1));
+            return num > 0 && firstLetter > 0;
         } catch (NumberFormatException e) {
             try {
                 double num = Double.parseDouble(str);
